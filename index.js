86642c3dc06fbb2b282f281d243cdc9fa55dd399
jebastinherlen <herlenjebastin@gmail.com>
2026-04-25 23:56:29 +0530

    added forgot password fields to user schema

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
 app.use('/api/earphones',earphoneRouter);
 app.use('/api/users',userRoutes);
 app.use('/api/orders',orderRouter)
 
-app.listen (port,()=>{
-    console.log(`server is running on localhost:${port}`)
+// app.listen (port,()=>{
+//     console.log(`server is running on localhost:${port}`)
     
-})
+// })
 
 
 
--- models/usermodel.js
+++ models/usermodel.js
@@ -10,6 +10,8 @@ const userSchema = new mongoose.Schema(
       enum: ["admin", "user"],
       default: "user",
     },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
   },
   { timestamps: true }
 );
